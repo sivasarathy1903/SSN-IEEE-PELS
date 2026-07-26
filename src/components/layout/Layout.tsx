05f4c7e0c79e1dac633c627a91d91d1636cf3cae
Sivasarathy A <sivasarathy2007@gmail.com>
2026-07-26 09:27:43 +0530

    fix: removed ScrollRestoration and added vercel.json for SPA routing

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
-import { ScrollRestoration } from "react-router-dom";
 
 export function Layout() {
   return (
     <div className="min-h-screen flex flex-col font-body-md overflow-x-hidden">
-      <ScrollRestoration />
       <Navbar />
       <main className="flex-grow pt-20">
         <Outlet />
